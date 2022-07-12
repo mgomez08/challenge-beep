2fa24b2e05a41a1445dcb66e5fac3b31cc23c813
Marlon Gómez <marlonjulian15@gmail.com>
2022-07-12 12:15:21 -0500

    Create auth types

---
+++ src/types/index.js
@@ -0,0 +1,8 @@
+const types = {
+  AUTH_REGISTER: "[AUTH] Register",
+  AUTH_LOGIN: "[AUTH] Login",
+  AUTH_CHECKING_FINISH: "[AUTH] Checking Finish",
+  AUTH_LOGOUT: "[AUTH] Logout",
+};
+
+export default types;
