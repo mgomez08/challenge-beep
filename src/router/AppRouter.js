5d3672f0e11196f62bf14edddc58ba840f1119ff
Marlon Gómez <marlonjulian15@gmail.com>
2022-07-12 12:17:56 -0500

    Create AppRouter

---
+++ src/router/AppRouter.js
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { checkingFinish, login } from "../actions/auth";
+import { PublicRoute } from "./PublicRoute";
+import { PrivateRoute } from "./PrivateRoute";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Home from "../pages/Home";
+
+const AppRouter = () => {
+  const dispatch = useDispatch();
+  const { checking, logged } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      user ? dispatch(login(user)) : dispatch(checkingFinish());
+    });
+  }, [dispatch]);
+  if (checking) return <div>Loading...</div>;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute restricted={true} logged={logged}>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicRoute restricted={true} logged={logged}>
+              <Register />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute logged={logged}>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
