51c691043914db712413fd236b09c40c5bb9950b
Marlon Gómez <marlonjulian15@gmail.com>
2022-07-12 12:14:55 -0500

    Add Provider store to app

--- src/BeepApp.js
+++ src/BeepApp.js
@@ -1,8 +1,14 @@
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import AppRouter from "./router/AppRouter";
+
 function BeepApp() {
   return (
-    <div className="App">
-      <h1>hola</h1>
-    </div>
+    <>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </>
   );
 }
 
