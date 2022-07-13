318fc4fa615d0fba21e101b5f266f6194a9ac1f6
Marlon Gómez <marlonjulian15@gmail.com>
2022-07-13 12:25:55 -0500

    Use TableMovie Components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import SearchMovies from "../components/Home/SearchMovies/SearchMovies";
+import TableMovies from "../components/Home/TableMovies/TableMovies";
 
 const Home = () => {
+  const { movies } = useSelector((state) => state.movies);
   return (
-    <main className="container d-flex justify-content-center mt-3">
+    <main className="container d-flex flex-column align-items-center mt-3">
       <SearchMovies />
+      {movies.length > 0 ? <TableMovies /> : <p>No hay películas agregadas</p>}
     </main>
   );
 };
