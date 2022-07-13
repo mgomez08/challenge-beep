e8744d8723751b226cb442bd20e7fc65922180b3
Marlon Gómez <marlonjulian15@gmail.com>
2022-07-13 11:22:54 -0500

    Add moviesReducer to rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./authReducer";
+import { moviesReducer } from "./moviesReducer";
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  movies: moviesReducer,
 });
