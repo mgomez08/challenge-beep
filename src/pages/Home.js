ce8dd5139c09f1ca89974fed159a158519a39c8d
Marlon Gómez <marlonjulian15@gmail.com>
2022-07-13 11:03:57 -0500

    Use SearchMovies component in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import { logout } from "../actions/auth";
+import SearchMovies from "../components/Home/SearchMovies/SearchMovies";
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const handleLogout = () => {
-    dispatch(logout());
-  };
   return (
-    <React.Fragment>
-      <h1>home</h1>
-      <button onClick={handleLogout}>Logout</button>
-    </React.Fragment>
+    <main className="container d-flex justify-content-center mt-3">
+      <SearchMovies />
+    </main>
   );
 };
 
